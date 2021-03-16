c4f8cbdcf364655083280d2d39bc6898d6523e6c
moony-bilcherry <erchinskaya.nataliya@gmail.com>
2021-03-17 00:44:07 +0300

    лабка 3 все

--- java_lab02/src/by/belstu/it/yarchynskaya/Main.java
+++ java_lab02/src/by/belstu/it/yarchynskaya/Main.java
@@ -1,6 +1,15 @@
 package by.belstu.it.yarchynskaya;
 
+/**
+ * @author moony
+ * @version 1.0.0.1
+ * */
 public class Main {
+    /**
+     * @return ничего потому что войд
+     * @trows
+     * @param args - аргументы
+     * */
     public static void main(String[] args) {
         //TODO hey look ma
         System.out.println("Hiiii");
@@ -10,5 +19,22 @@ public class Main {
         }
 
         TestFunction obj = new TestFunction();
+
+        String elem = "hey";
+
+        WrapperString ws1 = new WrapperString(elem);
+        ws1.replace();
+
+        WrapperString ws2 = new WrapperString(elem) {
+            @Override
+            public void replace() {
+                System.out.println("метод replace от анона");
+                delete();
+            }
+            public void delete() {
+                System.out.println("я удалятель");
+            }
+        };
+        ws2.replace();
     }
 }
