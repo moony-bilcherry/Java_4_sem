cde8665797b834f424e125a5f21fee3b28cd7448
moony-bilcherry <erchinskaya.nataliya@gmail.com>
2021-03-16 23:29:45 +0300

    to 3g

--- java_lab02/src/by/belstu/it/yarchynskaya/basejava/JavaTest.java
+++ java_lab02/src/by/belstu/it/yarchynskaya/basejava/JavaTest.java
@@ -14,7 +14,147 @@ public class JavaTest {
         EX_3b();
         EX_3c();
         EX_3d();
+        EX_3e();
+        EX_3f();
+        EX_3g();
+    }
 
+    private static void EX_3g() {
+        System.out.println("\t\tEX 3g");
+        char[][] c1;
+        char[] c2[];
+        char c3[][];
+
+        int zero_arr[] = new int [0];
+        // исключение: выход за пределы
+        //System.out.println(zero_arr[5]);
+
+        // c1
+        c1 = new char[3][];
+        c1[0] = new char[0];
+        c1[1] = new char[1];
+        c1[2] = new char[2];
+        System.out.println("длина массива: " + c1.length);
+        System.out.println("длина строки 0: " + c1[0].length);
+        System.out.println("длина строки 1: " + c1[1].length);
+        System.out.println("длина строки 2: " + c1[2].length);
+
+        c2 = new char[][] {{'1','2','3'}, {'4','5','6'}, {'7','8','9'}};
+        c3 = new char[][] {{'1','2','3'}, {'4','5','6'}, {'7','8','9'}};
+        boolean comRez = c2 == c3;
+        System.out.println(comRez);
+        c2 = c3;
+        System.out.println(c2 == c3);
+        for (char[] inner_arr: c2) {
+            for (char cell: inner_arr) {
+                System.out.println(cell);
+            }
+        }
+    }
+
+    private static void EX_3f() {
+        System.out.println("\t\tEX 3f");
+        String s34 = "2345";
+
+        // преобразование в инт:
+        System.out.println("Integer.valueOf(s34): " + (Integer.valueOf(s34)));
+        System.out.println("new Integer(s34): " + (new Integer(s34)));
+        System.out.println("Integer.parseInt(s34): " + (Integer.parseInt(s34)));
+
+        // перевод строки в массив байтов и обратно из массива байтов в строку
+        byte[] str_arr = s34.getBytes();
+        System.out.println("массив байтов из строки: " + str_arr);
+
+        String new_s34 = new String(str_arr);
+        System.out.println("из байтов в строку: " + new_s34);
+
+        // строка в логический тип 2мя способами
+        System.out.println("1: " + Boolean.valueOf(s34));
+        System.out.println("2: " + Boolean.getBoolean(s34));
+        System.out.println("3: " + Boolean.parseBoolean(s34));
+
+        // сравнения
+        String str1 = "meow";
+        String str2 = "meow";
+        System.out.println("== : " + (str1 == str2));
+        System.out.println("equals: " + (str1.equals(str2)));
+        System.out.println("compareTo: " + (str1.compareTo(str2)));
+        str2 = null;
+        System.out.println("== : " + (str1 == str2));
+        System.out.println("equals: " + (str1.equals(str2)));
+        // исключение
+        //System.out.println("compareTo: " + (str1.compareTo(str2)));
+
+        // функции split, contains, hashCode, indexOf, length, replace
+        String my_str = "i'm out of my head";
+        System.out.println("оригинал строки: " + my_str);
+        String[] my_split = my_str.split(" ");
+        for (String a : my_split)
+            System.out.println(a);
+        System.out.println("hashCode: " + my_str.hashCode());
+        System.out.println("indexOf: " + my_str.indexOf("of"));     // индекс первого символа подстроки в строке
+        System.out.println("length: " + my_str.length());
+        System.out.println("replace: " + my_str.replace("head", "mind"));
+        System.out.println();
+    }
+
+    private static void EX_3e() {
+        System.out.println("\t\tEX 3e");
+        Boolean o_bool = new Boolean(true);
+        Character o_char = new Character('Q');
+        Integer o_int = new Integer(391);
+        Byte o_byte = new Byte((byte) 56);
+        Short o_short = new Short((short) 268);
+        Long o_long = new Long(7453432462L);
+        Double o_double = new Double(543.2);
+
+        // >>>, >>, ~, &, *, -, +
+        System.out.println("\tОператоры:");
+        System.out.println("395 >>> 1 (беззнаковый сдвиг): " + (o_int >>> 1));
+        System.out.println("-10 >>> 1 (беззнаковый сдвиг): " + (-10 >>> 1));
+        System.out.println("395 >> 1 (знаковый сдвиг): " + (o_int >> 1));
+        System.out.println("-10 >> 1 (знаковый сдвиг): " + (-10 >> 1));
+        System.out.println("~125 (не): " + (~(125)));
+        System.out.println("127 & 11 = " + (127 & 11));
+        System.out.println("127 | 11 = " + (127 | 11));
+        System.out.println("127 ^ 11 = " + (127 ^ 11));
+        System.out.println("true && true = " + (o_bool && true));
+        System.out.println("true && false = " + (o_bool && !o_bool));
+        System.out.println("true || true = " + (o_bool || !o_bool));
+
+        // MIN_VALUE и MAX_VALUE для Long и Double
+        System.out.println("\tLong, Double:");
+        System.out.println("Long.MAX_VALUE: " + (Long.MAX_VALUE));
+        System.out.println("Long.MIN_VALUE: " + (Long.MIN_VALUE));
+
+        System.out.println("o_double.isNaN(): " + (o_double.isNaN()));      // isNaN() только у Float и Double
+        System.out.println("Double.MAX_VALUE: " + (Double.MAX_VALUE));
+        System.out.println("Double.MIN_VALUE: " + (Double.MIN_VALUE));
+
+        // упаковка и распаковка для Integer и Byte
+        Integer boxed_int = 19;              // автоупаковка
+        int unbox_int = 287;
+        unbox_int = new Integer(12);    // распаковка
+
+        Byte boxed_byte = 16;
+        byte unbox_byte = 32;
+        unbox_byte = new Byte((byte)53);
+
+        //  для Integer методы : parseInt ; toHexString ; compare ; toString ; bitCount ; isNaN ...
+        System.out.println("\tInteger:");
+        System.out.println("ParseInt('170'): " + (Integer.parseInt("170")));
+        System.out.println("ParseInt('170', 16): " + (Integer.parseInt("170",16)));
+
+        System.out.println("toString(523): " + (Integer.toString(523)));
+        // число -> строка с числом в 16й сс
+        System.out.println("toHexString(742): " + (Integer.toHexString(742)));
+
+        System.out.println("compare(10, 17): " + (Integer.compare(10,17)));
+        System.out.println("compare(17, 17): " + (Integer.compare(17,17)));
+        System.out.println("compare(17, 10): " + (Integer.compare(17,10)));
+        System.out.println("o_int.compareTo(17): " + (o_int.compareTo(17)));
+
+        System.out.println("bitCount(127): " + (Integer.bitCount(127)));        // кол-во 1 битов числа в 2й сс
     }
 
     private static void EX_3d() {
@@ -30,6 +170,7 @@ public class JavaTest {
         double random = new Random().nextDouble();
         double result = start + (random * (end - start));
         System.out.println(result);
+        System.out.println();
     }
 
     private static void EX_3c() {
